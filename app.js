5b32dd5e6079a4539565487204d506c40ff001c5
Dallyla Oliveira <dallyla.s.oliveira@gmail.com>
2020-08-26 19:54:13 -0300

    add bcryp

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
-console.log(process.env.SECRET);
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -22,9 +22,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]  });
 
 
 const User = new mongoose.model("User", userSchema);
@@ -42,19 +39,24 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
 
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
     });
 });
+
+    
 
 app.post("/login", function(req, res){
     const username = req.body.username;
@@ -65,9 +67,14 @@ app.post("/login", function(req, res){
             console.log(err);
         }else {
             if(foundUser){
-                if (foundUser.password === password){
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result === true){
+                        res.render("secrets");
+                    }
+                    
+                });
+                
+                
                 
             }
             
